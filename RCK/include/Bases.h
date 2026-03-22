1061bd7ceb34e90925aeb499a2c0d0e6256fea40
Abstogo <abstogo@protonmail.com>
2026-03-22 06:47:11 +0000

    it's all so tiresome

--- RCK/include/Bases.h
+++ RCK/include/Bases.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <fstream>
 #include <jsoncons/json.hpp>
-#include <jsoncons/json_traits_macros.hpp>
+#include <jsoncons/json_type_traits.hpp>
 #include <jsoncons_ext/csv/csv.hpp>
 
 #include "Character.h"
